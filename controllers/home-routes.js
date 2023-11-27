a842011ac6f8e035f5291dc7ec3a98d3a4674674
RobertWright0830 <dnafrankenstein@gmail.com>
2023-11-26 20:44:47 -0600

    module builds, model setup, routes setup, helper setup

---
+++ controllers/home-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Gallery, Painting } = require('../models');
+
+// GET all posts for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ['title', 'content', 'user_id', 'post_date'],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) =>
+      post.get({ plain: true })
+    );
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// // GET one gallery
+// router.get('/gallery/:id', async (req, res) => {
+//   try {
+//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Painting,
+//           attributes: [
+//             'id',
+//             'title',
+//             'artist',
+//             'exhibition_date',
+//             'filename',
+//             'description',
+//           ],
+//         },
+//       ],
+//     });
+
+//     const gallery = dbGalleryData.get({ plain: true });
+//     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// // GET one painting
+// router.get('/painting/:id', async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//     const painting = dbPaintingData.get({ plain: true });
+//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
